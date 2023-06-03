25e0c979b0b26d78e197d62978529d86b4ab3783
As-swiddeeq <sideeqolabamiji0@gmail.com>
2023-06-03 02:01:16 +0100

    All done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["","Frontend Developer","Office/Information Manager","Personal/Virtual Assistant","Secretary","Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
